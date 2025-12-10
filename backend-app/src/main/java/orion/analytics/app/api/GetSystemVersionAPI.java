5434d279345c1c9423fe4ddc9e63d628478f09e9
Dimitrios Efthymiou <dimitrios.efthymiou@yapily.com>
2025-12-10 19:15:45 +0000

    1

--- backend-app/src/main/java/orion/analytics/app/api/GetSystemVersionAPI.java
+++ backend-app/src/main/java/orion/analytics/app/api/GetSystemVersionAPI.java
@@ -5,23 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import orion.analytics.core.api.APIResponse;
+import orion.analytics.core.configuration.Configuration;
 
 @RestController
 public class GetSystemVersionAPI
 {
-    @Value("${orion.version}") private String systemVersion;
-
-
     @GetMapping(value = "/system/version")
     public ResponseEntity<APIResponse> getSystemVersion()
     {
         APIResponse<Version> response = new APIResponse<>(Version.builder()
-                        .version(systemVersion)
+                        .version((String)Configuration.getProp("orion.version"))
                         .build());
         return ResponseEntity.ok(response);
     }
